22d3d50696d1afb9f8c7c8937ef2326a070f235a
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-19 11:27:27 -0500

    update

--- myapp/components/Cards/GalleryCard/GalleryCard.js
+++ myapp/components/Cards/GalleryCard/GalleryCard.js
@@ -1,14 +1,17 @@
 import getServerUser from '@/lib/getServerUser';
 import Link from 'next/link';
-import { FaHeart, FaEdit, FaTrash } from 'react-icons/fa';
 import { Types } from 'mongoose';
+import { FiHeart, FiEdit, FiTrash, FiEye } from 'react-icons/fi';
 
-const GalleryCard = ({ imageUrl, title, desc, index, isFavorite, isCurrentUser, creator, postUserId }) => {
+
+const GalleryCard = ({ imageUrl, title, desc, index, isFavorite, isCurrentUser, creator: { _id: creatorId, avatar: creatorAvatar, email: creatorEmail }, postUserId }) => {
   const user = getServerUser();
   const userId = user ? new Types.ObjectId(user._id) : undefined;
-  const creatorID = creator ? new Types.ObjectId(creator._id) : undefined; // Assuming the creator object has an _id property
-  const creatorAvatar = creator ? creator.avatar : ''; // Assuming the creator object has an avatar property
+  const creatorID = creatorId ? new Types.ObjectId(creatorId) : undefined; // Assuming the creator object has an _id property
 
+  console.log("First Session " + user)
+  console.log(creatorEmail)
+  console.log(creatorId)
   console.log("UserID: ", userId);
   console.log("ImageUrl: ", imageUrl);
   console.log("CreatorID: ", creatorID);
@@ -16,46 +19,38 @@ const GalleryCard = ({ imageUrl, title, desc, index, isFavorite, isCurrentUser, 
 
   const renderHeartIcon = () => {
     if (isFavorite) {
-      return <FaHeart size={20} style={{ color: 'red' }} />;
+      return <Fi    Heart size={20} style={{ color: 'red' }} />;
     } else {
-      return <FaHeart size={20} />;
+      return <FiHeart size={20} />;
     }
   };
 
-  const renderEditDeleteIcons = () => {
-    if (isCurrentUser && userId && creatorID && userId.equals(creatorID)) {
-      return (
-        <div className='flex justify-between w-full'>
-          <button>{renderHeartIcon()}</button>
-          <button><FaEdit size={20} /></button>
-          <button><FaTrash size={20} /></button>
-        </div>
-      );
-    } else {
-      return <div>{renderHeartIcon()}</div>;
-    }
-  };
+
 
   return (
     <Link href={`/`}>
-      <div
-        key={index}
-        className='relative group bg-primary h-96 overflow-hidden rounded-md transition-transform transform hover:scale-105'
-      >
-        <img src={imageUrl} className='h-full' />
-        <div className='absolute top-0 left-0 right-0 p-4 flex justify-between items-center opacity-0 group-hover:opacity-100 transition-opacity'>
-          {renderEditDeleteIcons()}
-        </div>
+<div
+  key={index}
+  className='relative group bg-primary h-96 overflow-hidden rounded-md transition-transform transform hover:scale-105'
+>
+  <img src={imageUrl} className='h-full' />
+  <div className='dark-overlay absolute top-0 left-0 right-0 bottom-0'></div> {/* Dark overlay */}
+  <div className='text-white absolute top-0 left-0 right-0 p-4 flex justify-center gap-8 items-center opacity-0 group-hover:opacity-100 transition-opacity'>
+  <FiHeart size={20} />
+  <FiEye size={20} />
 
-        <div className='absolute bottom-0 left-0 right-0 p-4 flex items-end opacity-0 group-hover:opacity-100 transition-opacity'>
-          <div className='mr-2'>
-            <img src={creatorAvatar} alt='Profile' className='w-8 h-8 rounded-full' />
-          </div>
-          <div>
-            <p className='text-white font-semibold'>{title}</p>
-          </div>
-        </div>
-      </div>
+</div>
+
+  <div className='absolute bottom-0 left-0 right-0 p-4 flex items-end opacity-0 group-hover:opacity-100 transition-opacity'>
+    <div className='mr-2'>
+      <img src={creatorAvatar} alt='Profile' className='w-8 h-8 rounded-full' />
+    </div>
+    <div>
+      <p className='text-white font-semibold'>{title}</p>
+    </div>
+  </div>
+</div>
+
     </Link>
   );
 };
